f3628b88ed0c5d01d9544f09bdddc7f711b43b0f
Serhii-Lukhin <124396405+Serhii-Lukhin@users.noreply.github.com>
2023-03-10 13:28:59 +0200

    css+java
    нове меню
    прокрутка

--- java/script.js
+++ java/script.js
@@ -1,9 +1,9 @@
 const MENU_ACTIVE_CLASS_NAME = "visible-element";
-const ICON_HIDDEN_CLASS_NAME = "icon-hidden";
-const ICON_VISIBLE_CLASS_NAME = "icon-visible";
+const BODY_FiXED_CLASS_NAME = "stop-scroll";
 
 /*menu*/
 const myMenu = document.querySelector("#my-menu");
+const myBody = document.body;
 
 const openMenuBtn = document.querySelector("#open-menu-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
@@ -12,41 +12,29 @@ const closeMenuBtns = document.querySelectorAll(".link-menu");
 openMenuBtn.addEventListener("click", () => {
   myMenu.classList.add(MENU_ACTIVE_CLASS_NAME);
 });
-
 openMenuBtn.addEventListener("click", () => {
-  closeMenuBtn.classList.add(ICON_VISIBLE_CLASS_NAME);
-});
-
-openMenuBtn.addEventListener("click", () => {
-  openMenuBtn.classList.add(ICON_HIDDEN_CLASS_NAME);
+  myBody.classList.add(BODY_FiXED_CLASS_NAME);
 });
 
 closeMenuBtn.addEventListener("click", () => {
   myMenu.classList.remove(MENU_ACTIVE_CLASS_NAME);
 });
 closeMenuBtn.addEventListener("click", () => {
-  closeMenuBtn.classList.remove(ICON_VISIBLE_CLASS_NAME);
-});
-closeMenuBtn.addEventListener("click", () => {
-  openMenuBtn.classList.remove(ICON_HIDDEN_CLASS_NAME);
+  myBody.classList.remove(BODY_FiXED_CLASS_NAME);
 });
 
 const closeMyMenu = () => {
   myMenu.classList.remove(MENU_ACTIVE_CLASS_NAME);
 };
-const hideCloseBtn = () => {
-  closeMenuBtn.classList.remove(ICON_VISIBLE_CLASS_NAME);
-};
-const showMenuBtn = () => {
-  openMenuBtn.classList.remove(ICON_HIDDEN_CLASS_NAME);
+const startScrolling = () => {
+  myBody.classList.remove(BODY_FiXED_CLASS_NAME);
 };
 
 closeMenuBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.stopPropagation();
     closeMyMenu();
-    hideCloseBtn();
-    showMenuBtn();
+    startScrolling();
   });
 });
 
@@ -60,9 +48,34 @@ const closeModalBtn = document.querySelector("#close-mail-form");
 openModalBtn.addEventListener("click", () => {
   myMailForm.classList.add(MENU_ACTIVE_CLASS_NAME);
 });
+openModalBtn.addEventListener("click", () => {
+  myBody.classList.add(BODY_FiXED_CLASS_NAME);
+});
+
 footerOpenModalBtn.addEventListener("click", () => {
   myMailForm.classList.add(MENU_ACTIVE_CLASS_NAME);
 });
+footerOpenModalBtn.addEventListener("click", () => {
+  myBody.classList.add(BODY_FiXED_CLASS_NAME);
+});
+
 closeModalBtn.addEventListener("click", () => {
   myMailForm.classList.remove(MENU_ACTIVE_CLASS_NAME);
+});
+
+closeModalBtn.addEventListener("click", () => {
+  myBody.classList.remove(BODY_FiXED_CLASS_NAME);
+});
+
+/*button-up*/
+const myHeader = document.header;
+const myButtonUp = document.querySelector("#my-button-up");
+
+window.addEventListener("scroll", function () {
+  let myScrollPx = window.pageYOffset;
+  if (myScrollPx > 70) {
+    myButtonUp.classList.add(MENU_ACTIVE_CLASS_NAME);
+  } else {
+    myButtonUp.classList.remove(MENU_ACTIVE_CLASS_NAME);
+  }
 });
